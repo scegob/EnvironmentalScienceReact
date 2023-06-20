2ea9a9184f7725c53e43d49d19bf186679807de9
Billeh <scegob@gmail.com>
2023-06-19 19:47:24 -0700

    first

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+        <a className ="title" href="#">Environmental Science</a>
+      <ul className="top">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Unit Overview</a>
+        </li>
+        <li>
+          <a href="#">Why</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
